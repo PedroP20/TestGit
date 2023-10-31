ef850446e42a013deef32a13de05ca43ef72ddca
Pedro Euphrausino <pedropauloeph@gmail.com>
2023-10-31 17:04:52 -0300

    JS3

--- Projeto/js/script.js
+++ Projeto/js/script.js
@@ -7,3 +7,8 @@ document.getElementById("botao-new").addEventListener("click", function() {
     var caixaDeTexto = document.getElementById("caixaDeTexto-new");
     caixaDeTexto.textContent = "Texto Alterado!";
 });
+
+document.getElementById("botao-old").addEventListener("click", function() {
+    var caixaDeTexto = document.getElementById("caixaDeTexto-new");
+    caixaDeTexto.textContent = "Texto Alterado!";
+});
